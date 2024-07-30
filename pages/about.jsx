ef55501b6094e98eeaa84756fe76b676c113e2aa
shashi <shashi4868kumar@gmail.com>
2024-07-30 22:11:16 +0530

    Final commit

--- pages/about.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const About = () => {
-  return <div><h1>This is About page</h1></div>;
-};
---
+++ src/pages/about.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const About = () => {
+  return <div><h1>This is About page</h1></div>;
+};
